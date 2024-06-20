1e6d075cea18c151406a54c0ee8312c256b1cdbc
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-06-19 21:39:07 -0500

    buttons call functions

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ import homePage from './home_page.js';
 import menuPage from './menu_page.js';
 import './style.css';
 
-menuPage();
-console.log('test');
+const buttons = document.querySelectorAll('.button a');
+
+buttons.forEach(element => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault();
+        
+        let id = element.getAttribute('id');
+
+        switch(id){
+            case 'homeButton':
+                homePage();
+                break;
+            case 'menuButton':
+                menuPage();
+                break;
+            case 'contactButton':
+                contactPage();
+                break;
+        }
+    })
+});
